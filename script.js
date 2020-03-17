9c5df77a3bb3aa898a6ad53fdccd813218592ef8
Ivan Holub <ivanholub.js@gmail.com>
2020-03-17 12:46:49 +0200

    feat: implement phone screen on-off function

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ nav.addEventListener('click', function(event){
     })
     event.target.classList.add('activeNav');
 })
+
+
+//phone monitor on-off
+
+let phoneV = document.querySelector('.phone-vertical');
+let imageInsidePhoneVert = document.querySelector('.image__inside-v');
+let phoneH = document.querySelector('.phone-horizontal');
+let imageInsidePhoneHor = document.querySelector('.image__inside-h');
+
+phoneV.addEventListener('click', function(event){
+    imageInsidePhoneVert.classList.toggle('display-none');
+})
+
+phoneH.addEventListener('click', function(event){
+    imageInsidePhoneHor.classList.toggle('display-none');
+})
+
