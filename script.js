6506d039acbd0cfe3eec82ce0c89a2d9f1744508
Ivan Holub <ivanholub.js@gmail.com>
2020-03-17 13:23:35 +0200

    feat: implement active tags, photo moving, bordered photo

--- script.js
+++ script.js
@@ -27,3 +27,51 @@ phoneH.addEventListener('click', function(event){
     imageInsidePhoneHor.classList.toggle('display-none');
 })
 
+
+
+
+//active tags and photo moving
+
+let tagsMenu = document.querySelector('.tags');
+let tags = document.querySelectorAll('.tags li a');
+let photosPortfolio = document.querySelector('.photos');
+let photoPortfolio = document.querySelectorAll('.photos img');
+
+
+tagsMenu.addEventListener('click', function(event){
+    event.preventDefault();
+
+    tags.forEach(function(e){
+        e.classList.remove('activeTag');
+    })
+    event.target.classList.add('activeTag');
+
+    let imageArr = [];
+    photoPortfolio.forEach(function(el){
+         imageArr.push(el)
+    });
+    imageArr.sort(function(){
+        return 0.5 - Math.random();
+    })
+    photosPortfolio.innerHTML = '';
+    imageArr.forEach(function(e){
+        photosPortfolio.appendChild(e);
+    })
+
+})
+
+
+//photo bordered
+
+// .activePhoto 
+
+photosPortfolio.addEventListener('click', function(event){
+    
+    photoPortfolio.forEach(function(el){
+        el.classList.remove('activePhoto');
+    })
+    event.target.classList.add('activePhoto');
+    if(photosPortfolio.classList.contains('activePhoto')){
+        photosPortfolio.classList.remove('activePhoto')
+    }
+})
